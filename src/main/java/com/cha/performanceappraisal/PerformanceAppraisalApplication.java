5be8dc3307b4bda0d8002ea0ac74af0532513f9b
Mohammed Ayman <m.ayman@tamkeentech.sa>
2022-04-01 11:19:35 +0300

    initial commit

---
+++ src/main/java/com/cha/performanceappraisal/PerformanceAppraisalApplication.java
@@ -0,0 +1,13 @@
+package com.cha.performanceappraisal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAppraisalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceAppraisalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cha/performanceappraisal/PerformanceAppraisalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cha.performanceappraisal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceAppraisalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
