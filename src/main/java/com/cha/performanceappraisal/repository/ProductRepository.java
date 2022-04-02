bd5d4d4216dff5d770a1aeeb7f2faedae1680b44
mayman <m.ayman@tamkeentech.sa>
2022-04-02 15:57:56 +0300

    add method findByName in product repository

--- src/main/java/com/cha/performanceappraisal/repository/ProductRepository.java
+++ src/main/java/com/cha/performanceappraisal/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.cha.performanceappraisal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cha.performanceappraisal.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	Optional<Product> findByName(String name);
 }
